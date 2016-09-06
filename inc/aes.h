7eb718f9da4ee446b1541956d9731f74e4dc4bca
Hanspeter Portner <dev@open-music-kontrollers.ch>
2016-09-06 17:08:38 +0200

    make implementation thread-safe.
    * get rid of global variables by packing them into a struct.

--- inc/aes.h
+++ inc/aes.h
@@ -26,27 +26,45 @@ extern "C" {
   #define CMAC 1
 #endif
 
+typedef uint8_t aes_state_t[4][4];
+typedef struct _aes_t aes_t;
+
+struct _aes_t {
+	// state - array holding the intermediate results during decryption.
+	aes_state_t* state;
+
+	// The array that stores the round keys.
+	uint8_t RoundKey[176];
+
+	// The Key input to the AES Program
+	const uint8_t* Key;
+
+#if defined(CBC) && CBC
+	// Initial Vector used only for CBC mode
+	uint8_t Iv[KEYLEN];
+#endif
+};
+
 #if defined(ECB) && ECB
 
-void AES128_ECB_encrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
-void AES128_ECB_decrypt(const uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES128_ECB_encrypt(aes_t *aes, const uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES128_ECB_decrypt(aes_t *aes, const uint8_t* input, const uint8_t* key, uint8_t *output);
 
 #endif // #if defined(ECB) && ECB
 
 
 #if defined(CBC) && CBC
 
-void AES128_CBC_encrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
-void AES128_CBC_decrypt_buffer(uint8_t* output, const uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
+void AES128_CBC_encrypt_buffer(aes_t *aes, uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
+void AES128_CBC_decrypt_buffer(aes_t *aes, uint8_t* output, const uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
 
 /* These variants encrypt and decrypt the data block in-place.
  * The data block length MUST be a multiple of the algorithm block size (16 bytes) 
  * The return value will be non-zero if the length is incorrect.
  * For the decypt function, the iv data must be writable, and will be modified on return.
  */
-uint8_t AES128_CBC_encrypt_inplace( uint8_t* data, size_t length, const uint8_t* key, const uint8_t* iv);
-uint8_t AES128_CBC_decrypt_inplace( uint8_t* data, size_t length, const uint8_t* key, const uint8_t* iv);
-
+uint8_t AES128_CBC_encrypt_inplace(aes_t *aes, uint8_t* data, size_t length, const uint8_t* key, const uint8_t* iv);
+uint8_t AES128_CBC_decrypt_inplace(aes_t *aes, uint8_t* data, size_t length, const uint8_t* key, const uint8_t* iv);
 
 #endif // #if defined(CBC) && CBC
 
