f433edc331eb393f253ce74799c6d5060fa2e4d0
Ajay Bhargav <contact@rickeyworld.info>
2017-01-23 10:51:12 +0530

    Remove build warning
    src/test/test.c:226:3: warning: implicit declaration of function
    'AES128_CMAC_generate_subkey' [-Wimplicit-function-declaration]
    
    Signed-off-by: Ajay Bhargav <contact@rickeyworld.info>

--- inc/aes.h
+++ inc/aes.h
@@ -58,6 +58,7 @@ uint8_t AES128_CBC_decrypt_inplace( uint8_t* data, size_t length, const uint8_t*
 #if defined(CMAC) && CMAC
 
 void AES128_CBC_encrypt_block(uint8_t* output, uint8_t* input, const uint8_t* key);
+void AES128_CMAC_generate_subkey(uint8_t* K1, uint8_t* K2, const uint8_t* key);
 void AES128_CMAC(uint8_t* mac, uint8_t* message, uint32_t msgLen, uint8_t* key);
 
 #endif // #if defined(CMAC) && CMAC
